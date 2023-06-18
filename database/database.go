74b30ae8efb6606af2e84368d62143eb1e7c6569
Dwi Andhika <dwiandhikaaji@gmail.com>
2023-06-18 16:12:40 +0700

    use different dsn for each env

--- database/database.go
+++ database/database.go
@@ -9,6 +9,7 @@ import (
 	"gorm.io/gorm/logger"
 
 	"csbackend/models"
+	"csbackend/util"
 
 	"os"
 )
@@ -20,11 +21,11 @@ func New() (*gorm.DB, error) {
 	//dbname := os.Getenv("PGDATABASE")
 	//port := os.Getenv("PGPORT")
 
-	/* dsn := os.Getenv("DSN_DEV")
+	dsn := os.Getenv("DSN_DEV")
 	if util.IsProduction() {
 		dsn = os.Getenv("DSN_PROD")
-	} */
-	dsn := os.Getenv("DSN")
+	}
+	//dsn := os.Getenv("DSN")
 	println(dsn)
 	//dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", host, user, password, dbname, port)
 
